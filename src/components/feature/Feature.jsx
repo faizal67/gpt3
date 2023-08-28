c6a32b85ddbc5abcc3459a31554b98a1723b2b8d
Md Faizal <faizalmohammd67@gmail.com>
2023-08-28 23:58:40 +0530

    almost complete

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './feature.css';
 
-const Feature = () => {
+const Feature = ({text,title}) => {
   return (
-    <div>
-      feature
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container__feature-text'>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-headding'>
+        <h1 className='gradient__text'>
+        Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-linkes_logo'>
+          <img src={gpt3Logo} alt="gpt3logo" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>links</h4>
+          <p>overance</p>
+          <p>overance</p>
+          <p>overance</p>
+          <p>overance</p> 
+        </div>
+      </div>
     </div>
   )
 }
