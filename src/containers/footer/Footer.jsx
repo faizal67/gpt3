1544f082f2b4b6d50b8203525232a1ea35ae990b
Md Faizal <faizalmohammd67@gmail.com>
2023-08-30 00:21:32 +0530

    frontend complete

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -5,7 +5,7 @@ import gpt3Logo from '../../assets/logo.svg'
 const Footer = () => {
   return (
     <div className='gpt3__footer section__padding'>
-      <div className='gpt3__footer-headding'>
+      <div className='gpt3__footer-heading'>
         <h1 className='gradient__text'>
         Do you want to step in to the future before others
         </h1>
@@ -15,7 +15,7 @@ const Footer = () => {
       </div>
 
       <div className='gpt3__footer-links'>
-        <div className='gpt3__footer-linkes_logo'>
+        <div className='gpt3__footer-links_logo'>
           <img src={gpt3Logo} alt="gpt3logo" />
           <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
         </div>
@@ -23,10 +23,26 @@ const Footer = () => {
         <div className='gpt3__footer-links_div'>
           <h4>links</h4>
           <p>overance</p>
-          <p>overance</p>
-          <p>overance</p>
-          <p>overance</p> 
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p> 
         </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>overance</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>© 2021 GPT-3. All rights reserved.</p>
       </div>
     </div>
   )
